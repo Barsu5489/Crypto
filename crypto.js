61d3eefdeb93cabe8143a04c09a2571efad1fe2b
barsu5489 <ebarsulai@gmail.com>
2022-12-15 23:16:40 +0300

    fixed up coin id error

--- crypto.js
+++ crypto.js
@@ -1,19 +1,5 @@
 const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 const urlCoinDetails = 'https://api.coingecko.com/api/v3/coins/bitcoin'
-let container = document.createElement('div')
-container.className ='container'
-let body =  document.getElementById('body')
-body.appendChild(container)
-let header = document.createElement('div')
-header.className = 'header'
-let row = `  <p class="p">#</p>
-<p class="p">coin</p>
-<p class="p">price</p>
-<p class="p">time</p>
-<p class="p">Volume</p>
-<p class="p">Market cap</p>`
-header.insertAdjacentHTML('beforeend', row);
-container.appendChild(header)
 function fetchCoins()
 {
     fetch(url)
@@ -27,6 +13,20 @@ function fetchCoins()
 
 function renderCoins(coin)
 {
+    const container = document.createElement('div')
+    container.className ='container'
+   const body =  document.getElementById('body')
+   body.appendChild(container)
+   const header = document.createElement('div')
+   header.className = 'header'
+    const row = `  <p class="p">#</p>
+    <p class="p">coin</p>
+    <p class="p">price</p>
+    <p class="p">time</p>
+    <p class="p">Volume</p>
+    <p class="p">Market cap</p>`
+    header.insertAdjacentHTML('beforeend', row);
+    container.appendChild(header)
     coin.forEach(coin=>{
 
         
@@ -65,17 +65,16 @@ function renderCoins(coin)
   //const coinRoww = document.querySelector('c-row');
   cRow.addEventListener('click',(e)=>
   {
-    
+    container.style.display = 'none'
     //const coinId = coin.id
-    const abs = e.target.className.substring(6)
+    const abs = cRow.className.substring(6)
     
     if(abs == coin.id)
     {
-        console.log(abs)
+       // console.log(abs)
         coinDetails(abs)
-        container.style.display = 'none'
-        
     }
+    else{console.log('err') }
     
   })
 })
@@ -89,8 +88,6 @@ function coinDetails(coinId)
     .then(data=>
         {
             console.log(data)
-            container.appendChild(header)
-
         },[])
 }
 // function renderDetails(data)
